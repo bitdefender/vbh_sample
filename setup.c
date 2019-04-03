d36acaa5452a9dbe5e96bfe851753c61a4c0feba
Bogdan-Viorel BOSINTA <bbosinta@bitdefender.com>
2019-04-03 23:03:38 +0300

    HVI sample module working with intel's VBH

---
+++ setup.c
@@ -0,0 +1,299 @@
+#include <linux/module.h>
+#include <linux/printk.h>
+#include <asm/processor-flags.h>
+#include <linux/sched.h>
+
+#include "hypervisor_introspection.h"
+
+#define CR4                                 4
+
+#define STRINGIFY(x)                        #x
+#define TOSTRING(x)                         STRINGIFY(x)
+#define KVI_LOAD_INTROSPECTION_HYPERCALL    41
+
+
+extern unsigned long long g_vdso_physical_address;
+int enable_vdso_protection(void);
+int disable_vdso_protection(void);
+
+int hvi_loaded = 0;
+int should_unload = 0;
+
+int loader(void);
+int unloader(void);
+
+int cr4_write_callback(hv_event_e type, unsigned char* data, int size, int *allow)
+{
+    struct hvi_event_cr *cr_event;
+
+    *allow = 1;         // Allow the action if an error occurs
+
+    if (type != cr_write)
+    {
+        printk(KERN_ERR "[ERROR] Invalid event type sent to cr4_write_callback: %d\n", type);
+        return 0;       // vbh doesn't check the error code so its fine
+    }
+
+    if (size < sizeof(struct hvi_event_cr))
+    {
+        printk(KERN_ERR "[ERROR] Invalid data size.\n");
+        return 0;
+    }
+
+    cr_event = (struct hvi_event_cr*)data;
+    if (cr_event->cr != CR4)
+    {
+        printk(KERN_ERR "[ERROR] Invalid CR register. Expected CR4, got CR%d\n", cr_event->cr);
+        return 0;
+    }
+
+    if (((X86_CR4_SMAP & cr_event->old_value) && !(X86_CR4_SMAP & cr_event->new_value)) ||
+        ((X86_CR4_SMEP & cr_event->old_value) && !(X86_CR4_SMEP & cr_event->new_value)))
+    {
+        *allow = 0;
+        // useless log, kernel is likely going to crash immediately after this :(
+        printk(KERN_ERR "[WARNING] Malicious activity detected. Process %s tried to disable SMAP/SMEP. Will deny!\n", current->comm);
+    }
+
+    return 0;
+}
+
+
+int handle_vmcall(hv_event_e type, unsigned char* data, int size, int *allow)
+{
+    if (type != vmcall)
+    {
+        printk(KERN_ERR "[ERROR] Invalid event type sent to vmcall handler: %d\n", type);
+        return 0;
+    }
+
+    if (!hvi_loaded)
+    {
+        printk(KERN_ERR "[INFO] Entered vmx-root. Will initialize the rest of hvi.\n");
+        return loader();
+    }
+    if (should_unload)
+    {
+        printk(KERN_ERR "[INFO] Entered vmx-root. Uninitializing...\n");
+        return unloader();
+    }
+
+    printk(KERN_ERR "[WARNING] Unhandled vmcall!\n");
+    return 0;
+}
+
+
+int handle_ept_violation(hv_event_e type, unsigned char* data, int size, int *allow)
+{
+    struct hvi_event_page_fault *ept_violation_event;
+    if (type != ept_violation)
+    {
+        printk(KERN_ERR "[ERROR] Invalid event type sent to ept_violation handler: %d\n", type);
+        return 0;
+    }
+
+    if (size < sizeof(struct hvi_event_page_fault))
+    {
+        printk(KERN_ERR "[ERROR] Invalid data size.\n");
+        return 0;
+    }
+
+    ept_violation_event = (struct hvi_event_page_fault*)data;
+
+    printk(KERN_ERR "ept violation. GPA = 0x%llx  GLA = 0x%llx.\n", ept_violation_event->gla, ept_violation_event->gpa);
+    if (ept_violation_event->gpa >= g_vdso_physical_address && ept_violation_event->gpa < g_vdso_physical_address + 2 * PAGE_SIZE)
+    {
+        printk(KERN_ERR "[WARNING] Malicious write into vdso. Will deny!\n");
+        *allow = 0;
+    }
+    else
+    {
+        printk(KERN_ERR "[INFO] Unhandled ept violation. Will allow!\n");
+        *allow = 1;
+    }
+
+    return 0;
+}
+
+
+int register_cr4_write_callback(void)
+{
+    struct hvi_event_callback cr4_event_callback;
+    int status;
+
+    cr4_event_callback.event = cr_write;
+    cr4_event_callback.callback = cr4_write_callback;
+
+    status = hvi_register_event_callback(&cr4_event_callback, 1);
+    if (status)
+    {
+        printk(KERN_ERR "[ERROR] hvi_register_event_callback failed for cr4_write_callback. Error: %d\n", status);
+    }
+
+    return status;
+}
+
+
+int register_vmcall_callback(void)
+{
+    struct hvi_event_callback vmcall_event_callback;
+    int status;
+
+    vmcall_event_callback.event = vmcall;
+    vmcall_event_callback.callback = handle_vmcall;
+
+    status = hvi_register_event_callback(&vmcall_event_callback, 1);
+    if (status)
+    {
+        printk(KERN_ERR "[ERROR] hvi_register_event_callback failed for vmcall_event. Error: %d\n", status);
+    }
+
+    return status;
+}
+
+
+int register_ept_violation_callback(void)
+{
+    struct hvi_event_callback ept_violation_event_callback;
+    int status;
+
+    ept_violation_event_callback.event = ept_violation;
+    ept_violation_event_callback.callback = handle_ept_violation;
+
+    status = hvi_register_event_callback(&ept_violation_event_callback, 1);
+    if (status)
+    {
+        printk(KERN_ERR "[ERROR] hvi_register_event_callback failed for ept_violation_callback. Error: %d\n", status);
+    }
+
+    return status;
+}
+
+
+int enable_cr4_exits(void)
+{
+    hvi_modify_cr_write_exit(CR4, ~(unsigned int)0, 1);
+
+    return 0;
+}
+
+
+int disable_cr4_exits(void)
+{
+    hvi_modify_cr_write_exit(CR4, ~(unsigned int)0, 0);
+
+    return 0;
+}
+
+
+int loader(void)
+{
+    int status = 0;
+
+    status = register_cr4_write_callback();
+    if (status)
+    {
+        return status;
+    }
+
+    status = register_ept_violation_callback();
+    if (status)
+    {
+        goto _done_unregister_cr4_callback;
+    }
+
+    if (hvi_request_vcpu_pause())
+    {
+        goto _done_unregister_ept_violation_callback;
+    }
+
+    status = enable_cr4_exits();
+    if (status)
+    {
+        goto _done_resume_vcpus;
+    }
+
+    enable_vdso_protection();
+
+    hvi_loaded = 1;
+
+    printk(KERN_ERR "[INFO] Hvi successfully initialized!\n");
+
+    status = 0;
+
+_done_resume_vcpus:
+    if (hvi_request_vcpu_resume())
+    {
+        printk(KERN_ERR "Could not resume vcpus!\n");
+    }
+
+    if (0 == status)
+    {
+        return status;
+    }
+
+_done_unregister_ept_violation_callback:
+    hvi_unregister_event_callback(ept_violation);
+
+_done_unregister_cr4_callback:
+    hvi_unregister_event_callback(cr_write);
+
+    return status;
+}
+
+
+int unloader(void)
+{
+    hvi_request_vcpu_pause();
+
+    disable_cr4_exits();
+    disable_vdso_protection(); // We do this here because EPT paging structures are not available in vmx non-root.
+
+    hvi_request_vcpu_resume();
+
+    return 0;
+}
+
+
+static int __init hvi_init(void)
+{
+    // little big hack ingoming
+    // register the VMCALL handler, then issue a vmcall to enter vmx root
+
+    printk(KERN_ERR "[INFO] Will register VMCALL callback.\n");
+
+    if (register_vmcall_callback())
+    {
+        printk(KERN_ERR "[ERORR] Failed to register vmcall handler. Will abort initialization.\n");
+        return 0;
+    }
+
+    printk(KERN_ERR "[INFO] VMCALL callback registered. Will now try to enter vmx-root.\n");
+
+    asm volatile("mov $" TOSTRING(KVI_LOAD_INTROSPECTION_HYPERCALL) ", %rax;"
+                 "vmcall;");
+    return 0;
+}
+
+
+static void __exit hvi_uninit(void)
+{
+    printk(KERN_ERR "[INFO] Uninitializing Hvi...\n");
+
+    should_unload = 1;
+    asm volatile("mov $" TOSTRING(KVI_LOAD_INTROSPECTION_HYPERCALL) ", %rax;"
+                 "vmcall;");
+
+    hvi_unregister_event_callback(vmcall);
+    hvi_unregister_event_callback(cr_write);
+    hvi_unregister_event_callback(ept_violation);
+
+    printk(KERN_ERR "[INFO] Hvi successfully uninitialized!\n");
+}
+
+
+module_init(hvi_init);
+module_exit(hvi_uninit);
+MODULE_LICENSE("GPL");
+MODULE_VERSION("0.1");
+MODULE_AUTHOR("Bogdan-Viorel BOSINTA <bbosinta@bitdefender.com>");
---
+++ vdso_protection.c
@@ -0,0 +1,311 @@
+#include <linux/printk.h>
+#include <linux/string.h>
+#include <linux/kallsyms.h>
+#include "hypervisor_introspection.h"
+
+#define LINUX_KERNEL_START kallsyms_lookup_name("_stext")
+
+#ifndef PD_P
+#define PD_P                      0x001
+#define PDP_PS                    0x080
+#endif
+
+#define STATUS_SUCCESS                       0
+#define STATUS_UNSUCCESSFUL                 -1
+#define STATUS_NO_MORE_MAPPING_STRUCTURES   -2
+#define STATUS_INVALID_INTERNAL_STATE       -4
+
+
+#ifndef PAGE_SIZE_4K
+#define PAGE_SIZE_4K (4 * 1024)
+#define PAGE_SIZE_2M (2 * 1024 * 1024)
+#define PAGE_SIZE_1G (1 * 1024 * 1024 * 1024)
+#endif
+
+#ifndef PAGE_SIZE
+#define PAGE_SIZE PAGE_SIZE_4K
+#endif
+
+#ifndef PML4_INDEX
+#define PML4_INDEX(a)             (unsigned int)(((a) & 0x0000ff8000000000) >> 39)
+#define PDP_INDEX(a)              (unsigned int)(((a) & 0x0000007fc0000000) >> 30)
+#define PD_INDEX(a)               (unsigned int)(((a) & 0x000000003fe00000) >> 21)
+#define PT_INDEX(a)               (unsigned int)(((a) & 0x00000000001ff000) >> 12)
+#endif
+
+#ifndef CLEAN_PHYS_ADDRESS64
+#define CLEAN_PHYS_ADDRESS64(x)         ((x) & 0x000FFFFFFFFFF000)
+#endif
+
+unsigned long long g_vdso_physical_address;
+
+struct va_translation
+{
+    unsigned long long virtual_address;
+    unsigned long long physical_address;
+    unsigned long long page_size;
+};
+
+static int hvi_translate_va(unsigned long long va, unsigned long long cr3, struct va_translation *translation) 
+{
+    int status;
+    
+    unsigned int pml4i, pdpi, pdi, pti;
+    unsigned long long *pml4, *pdp, *pd, *pt;
+    
+    pml4 = pdp = pd = pt = NULL;
+
+    translation->virtual_address = va;
+
+    pml4i = PML4_INDEX(va);
+    pdpi  = PDP_INDEX (va);
+    pdi   = PD_INDEX  (va);
+    pti   = PT_INDEX  (va);
+
+    status = hvi_physmem_map_to_host(CLEAN_PHYS_ADDRESS64(cr3), PAGE_SIZE, 0, (void**)&pml4);
+    if (status)
+        goto cleanup_and_leave;
+
+    if (!(pml4[pml4i] & PD_P)) 
+    {
+        status = STATUS_NO_MORE_MAPPING_STRUCTURES;
+        goto cleanup_and_leave;
+    }
+
+    status = hvi_physmem_map_to_host(CLEAN_PHYS_ADDRESS64(pml4[pml4i]), PAGE_SIZE, 0, (void**)&pdp);
+    if (status)
+        goto cleanup_and_leave;
+
+    if (!(pdp[pdpi] & PD_P)) 
+    {
+        status = STATUS_NO_MORE_MAPPING_STRUCTURES;
+        goto cleanup_and_leave;
+    }
+
+    if (pdp[pdpi] & PDP_PS)
+    {
+        translation->page_size = PAGE_SIZE_1G;
+        translation->physical_address = CLEAN_PHYS_ADDRESS64(pdp[pdpi]);
+        status = STATUS_SUCCESS;
+        goto cleanup_and_leave;
+    }
+
+    status = hvi_physmem_map_to_host(CLEAN_PHYS_ADDRESS64(pdp[pdpi]), PAGE_SIZE, 0, (void**)&pd);
+    if (status)
+        goto cleanup_and_leave;
+
+    if (!(pd[pdi] & PD_P))
+    {
+        status = STATUS_NO_MORE_MAPPING_STRUCTURES;
+        goto cleanup_and_leave;
+    }
+
+    if (pd[pdi] & PDP_PS)
+    {
+        translation->page_size = PAGE_SIZE_2M;
+        translation->physical_address = CLEAN_PHYS_ADDRESS64(pd[pdi]);
+        status = STATUS_SUCCESS;
+        goto cleanup_and_leave;
+    }
+
+    status = hvi_physmem_map_to_host(CLEAN_PHYS_ADDRESS64(pd[pdi]), PAGE_SIZE, 0, (void**)&pt);
+    if (status)
+        goto cleanup_and_leave;
+    
+    translation->page_size = PAGE_SIZE;
+    translation->physical_address = CLEAN_PHYS_ADDRESS64(pt[pti]);
+    status = STATUS_SUCCESS;
+
+cleanup_and_leave:
+
+    if (NULL != pml4)
+        hvi_physmem_unmap((void**)&pml4);
+
+    if (NULL != pdp)
+        hvi_physmem_unmap((void**)&pdp);
+    
+    if (NULL != pd)
+        hvi_physmem_unmap((void**)&pd);
+
+    if (NULL != pt)
+        hvi_physmem_unmap((void**)&pt);
+
+    return status;
+}
+
+
+static int _hvi_match_vdso(void* mapping)
+{
+    unsigned int delta;
+    int matches;
+
+    if (*(unsigned int*)mapping != 0x464c457f) // .ELF
+    {
+        return 0;
+    }
+
+    matches = 0;
+    for (delta = 0; delta < PAGE_SIZE - 0x30; delta++)
+    {
+        if (!memcmp((char*)mapping + delta, "clock_gettime", strlen("clock_gettime")))
+        {
+            matches++;
+            continue;
+        }
+
+        if (!memcmp((char*)mapping + delta, "gettimeofday", strlen("gettimeofday")))
+        {
+            matches++;
+            continue;
+        }
+
+        if (!memcmp((char*)mapping + delta, "getcpu", strlen("getcpu")))
+        {
+            matches++;
+            continue;
+        }
+
+        if (3 == matches)
+            break;
+    }
+
+    return (3 == matches);
+}
+
+
+static int _hvi_hook_vdso(void)
+{
+    int status;
+
+    if (0 == g_vdso_physical_address)
+        return STATUS_INVALID_INTERNAL_STATE;
+
+    printk(KERN_ERR "will hook  %llx\n", g_vdso_physical_address);
+
+    status = hvi_set_ept_page_protection(g_vdso_physical_address, 1, 0, 1, 0);
+    if (status)
+    {
+        printk(KERN_ERR "[ERROR] hvi_set_ept_page_protection failed with status: %x\n", status);
+        return status;
+    }
+    else 
+    {
+        printk(KERN_ERR "[INFO] successfully hooked first vdso page\n");
+    }
+
+
+    status = hvi_set_ept_page_protection(g_vdso_physical_address + PAGE_SIZE, 1, 0, 1, 0);
+    if (status)
+    {
+        printk(KERN_ERR "[ERROR] hvi_set_ept_page_protection failed with status: %x\n", status);
+        return status;
+    }
+    else 
+    {
+        printk(KERN_ERR "[INFO] Successfully hooked second vdso page\n");
+    }
+
+    return STATUS_SUCCESS;
+}
+
+
+int disable_vdso_protection(void)
+{
+    int status;
+
+    if (0 != g_vdso_physical_address)
+    {
+        printk(KERN_ERR "[INFO] Will unhook  %llx\n", g_vdso_physical_address);
+        status = hvi_set_ept_page_protection(g_vdso_physical_address, 1, 1, 1, 0);
+        if (status)
+        {
+            printk(KERN_ERR "[ERROR] hvi_set_ept_page_protection failed with status: %x\n", status);
+            return status;
+        }
+        else
+        {
+            printk(KERN_ERR "[INFO] Sucessfully unhooked first vdso page\n");
+        }
+
+        status = hvi_set_ept_page_protection(g_vdso_physical_address + PAGE_SIZE, 1, 1, 1, 1);
+        if (status)
+        {
+            printk(KERN_ERR "[ERROR] hvi_set_ept_page_protection failed with status: %x\n", status);
+            return status;
+        }
+        else
+        {
+            printk(KERN_ERR "[INFO] Sucessfully unhooked second vdso page\n");
+        }
+    }
+
+    return STATUS_SUCCESS;
+
+}
+
+static inline unsigned long __cr3_read(void)
+{
+    unsigned long long cr3;
+    asm volatile("mov %%cr3, %%rax; mov %%rax, %0;" :"=m" (cr3) :: "%rax");
+    return cr3;
+}
+
+int enable_vdso_protection(void)
+{
+    void* mapping;
+    struct va_translation tr;
+    int status;
+    unsigned long long delta;
+
+    tr.virtual_address = LINUX_KERNEL_START;
+    tr.page_size = PAGE_SIZE;
+    tr.physical_address = 0; 
+
+    for (;;)
+    {
+        status = hvi_translate_va(tr.virtual_address, __cr3_read(), &tr);
+        if (status)
+        {
+            printk(KERN_ERR "[ERROR] hvi_translate_va failed: %x\n", status);
+            return status;
+        }
+
+        for (delta = 0; delta < tr.page_size; delta += PAGE_SIZE)
+        {
+            int found = 0;
+            status = hvi_physmem_map_to_host(tr.physical_address + delta, PAGE_SIZE, 0, &mapping);
+            if (status)
+            {
+                printk(KERN_ERR "[ERROR] hvi_physmem_map_to_host failed: %x\n", status);
+                return status;
+            }
+
+            found = _hvi_match_vdso(mapping);
+
+            status = hvi_physmem_unmap((void**)&mapping);
+
+            if (found)
+            {
+                g_vdso_physical_address = tr.physical_address + delta;
+                printk(KERN_ERR "[INFO] Found vdso at %llx %llx\n", tr.virtual_address + delta, tr.physical_address + delta);
+                goto _hook_vdso;
+            }
+        }
+
+        tr.virtual_address += tr.page_size;
+    }
+
+    printk(KERN_ERR "[ERROR] Vdso not found...\n");
+    return status;
+
+_hook_vdso:
+    
+    printk(KERN_ERR "[INFO] Hooking vdso...\n");
+    status =  _hvi_hook_vdso();
+    if (status)
+    {
+        printk(KERN_ERR "[ERROR] Failed hooking vdso..\n");
+    }
+    return status;
+}
+
